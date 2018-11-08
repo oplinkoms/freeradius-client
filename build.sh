25b2e2fd59a1d716aba228b02cf5a6c3bb26fdb2
fandy <fdy-fan@163.com>
2018-11-08 16:03:28 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+####  check execute result
+function return_val()
+{
+    val=`echo $?`
+        if test $val -ne 0;then
+        echo "#########  `pwd` is ERROR..."
+        exit -1
+    fi
+}
+
 
 if [ -f Makefile ]
 then                
@@ -5,10 +17,13 @@ then
 	make clean     
 else                
 	echo "configure first"              
-	./configure             
+	./configure; return_val     
 fi
 
-make
+make; return_val
 
+sudo cp -f lib/.libs/libfreeradius-client.so* $INSTALL_PATH/lib; return_val
+sudo cp -pdf radius/etc/* $INSTALL_PATH/etc/radiusclient; return_val
 
+exit 0
 
